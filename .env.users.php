2487954136d4ccb745beef3360abe22f81a83aa9
kassiejgarcia <kassiejgarcia@gmail.com>
2022-02-16 05:57:32 +0000

    added database credentials

---
+++ .env.users.php
@@ -0,0 +1,9 @@
+<?php
+// This is the file that stores the MYSQL database name, server, username, and password
+// this file is meant to be hidden
+// This file is specific to the database we have that stores users information
+
+define('DB_SERVER', 'localhost');
+define('DATABASE', 'user_information');
+define('DB_USERNAME', 'users_db_username');
+define('DB_PASSWORD', 'UTSA2022');
