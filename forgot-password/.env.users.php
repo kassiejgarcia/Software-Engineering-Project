bb9374707ada490d1e127278d39f5ab7143bac93
kassiejgarcia <61328920+kassiejgarcia@users.noreply.github.com>
2022-03-17 15:00:00 -0500

    Delete .env.users.php

--- forgot-password/.env.users.php
+++
@@ -1,9 +0,0 @@
-<?php
-// This is the file that stores the MYSQL database name, server, username, and password
-// this file is meant to be hidden
-// This file is specific to the database we have that stores users information
-
-define('DB_SERVER', 'localhost');
-define('DATABASE', 'SoftwareEngineeringProject');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'UTSA2022se!');
