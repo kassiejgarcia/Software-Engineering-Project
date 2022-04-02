cd1c00467ba1ab7a905c36c321054f11bd9b32c9
root <root@ip-172-31-92-25.ec2.internal>
2022-04-02 07:12:33 +0000

    worked on footer

--- index.php
+++ index.php
@@ -49,40 +49,10 @@ include("includes/main.php");
             <div class="footer-nav__heading">Information</div>
             <ul class="footer-nav__list">
               <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">The brand</a>
+                <a href="" class="footer-nav__link">FAQ</a>
               </li>
               <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Local stores</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Customer service</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Privacy &amp; cookies</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Site map</a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="footer-nav__col footer-nav__col--whybuy">
-            <div class="footer-nav__heading">Why buy from us</div>
-            <ul class="footer-nav__list">
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Shipping &amp; returns</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Secure shipping</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Testimonials</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Award winning</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Ethical trading</a>
+                <a href="" class="footer-nav__link">Help</a>
               </li>
             </ul>
           </div>
@@ -91,70 +61,35 @@ include("includes/main.php");
             <div class="footer-nav__heading">Your account</div>
             <ul class="footer-nav__list">
               <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Sign in</a>
+                <a href="checkout.php" class="footer-nav__link">Sign in</a>
               </li>
               <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Register</a>
+                <a href="customer_register.php" class="footer-nav__link">Register</a>
               </li>
               <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">View cart</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">View your lookbook</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Track an order</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="#" class="footer-nav__link">Update information</a>
+                <a href="cart.php" class="footer-nav__link">View cart</a>
               </li>
             </ul>
           </div>
 
 
           <div class="footer-nav__col footer-nav__col--contacts">
-            <div class="footer-nav__heading">Contact details</div>
+            <div class="footer-nav__heading">Contact us!</div>
             <address class="address">
-            Head Office: Avenue Fashion.<br>
-            180-182 Regent Street, London.
+            1 UTSA Circle, San Antonio, TX 78249
           </address>
             <div class="phone">
-              Telephone:
+              Phone Number:
               <a class="phone__number" href="tel:0123456789">0123-456-789</a>
             </div>
             <div class="email">
               Email:
-              <a href="mailto:support@yourwebsite.com" class="email__addr">support@yourwebsite.com</a>
+              <a class="email__addr">kassie.garcia@my.utsa.edu</a>
             </div>
           </div>
 
         </div>
       </div>
-
-      <!-- <div class="banners">
-        <div class="container clearfix">
-
-          <div class="banner-award">
-            <span>Award winner</span><br> Fashion awards 2016
-          </div>
-
-          <div class="banner-social">
-            <a href="#" class="banner-social__link">
-            <i class="icon-facebook"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-twitter"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-instagram"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-pinterest-circled"></i>
-          </a>
-          </div>
-
-        </div>
-      </div> -->
 
 
         </div>
