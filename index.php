fc04a032d532ac96d17c4865bbf4967142174f44
root <root@ip-172-31-92-25.ec2.internal>
2022-04-02 07:19:01 +0000

    updated footer

--- index.php
+++ index.php
@@ -39,62 +39,9 @@ include("includes/main.php");
     </div><!-- row Ends -->
 
     </div><!-- container Ends -->
-    <!-- FOOTER -->
-    <footer class="page-footer">
-
-      <div class="footer-nav">
-        <div class="container clearfix">
-
-          <div class="footer-nav__col footer-nav__col--info">
-            <div class="footer-nav__heading">Information</div>
-            <ul class="footer-nav__list">
-              <li class="footer-nav__item">
-                <a href="" class="footer-nav__link">FAQ</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="" class="footer-nav__link">Help</a>
-              </li>
-            </ul>
-          </div>
-
-          <div class="footer-nav__col footer-nav__col--account">
-            <div class="footer-nav__heading">Your account</div>
-            <ul class="footer-nav__list">
-              <li class="footer-nav__item">
-                <a href="checkout.php" class="footer-nav__link">Sign in</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="customer_register.php" class="footer-nav__link">Register</a>
-              </li>
-              <li class="footer-nav__item">
-                <a href="cart.php" class="footer-nav__link">View cart</a>
-              </li>
-            </ul>
-          </div>
-
-
-          <div class="footer-nav__col footer-nav__col--contacts">
-            <div class="footer-nav__heading">Contact us!</div>
-            <address class="address">
-            1 UTSA Circle, San Antonio, TX 78249
-          </address>
-            <div class="phone">
-              Phone Number:
-              <a class="phone__number" href="tel:0123456789">0123-456-789</a>
-            </div>
-            <div class="email">
-              Email:
-              <a class="email__addr">kassie.garcia@my.utsa.edu</a>
-            </div>
-          </div>
-
-        </div>
-      </div>
-
-
-        </div>
-      </div>
-    </footer>
+<?php /* FOOTER */
+include("includes/footer.php");
+?>
 </body>
 
 </html>
