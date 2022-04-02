7f4d6298805097308a7fa0a68fccf7e4ca71c962
kassiejgarcia <61328920+kassiejgarcia@users.noreply.github.com>
2022-04-02 00:43:32 -0500

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -71,18 +71,17 @@
 
 
 			<div class="footer-nav__col footer-nav__col--contacts">
-				<div class="footer-nav__heading">Contact details</div>
+				<div class="footer-nav__heading">Contact Us!</div>
 				<address class="address">
-				Head Office: Avenue Fashion.<br>
-				180-182 Regent Street, London.
+				1 UTSA Circle, San Antonio, TX 78249
 			</address>
 				<div class="phone">
-					Telephone:
+					Phone Number:
 					<a class="phone__number" href="tel:0123456789">0123-456-789</a>
 				</div>
 				<div class="email">
 					Email:
-					<a href="mailto:support@yourwebsite.com" class="email__addr">support@yourwebsite.com</a>
+					<a class="email__addr">kassie.garcia@my.utsa.edu</a>
 				</div>
 			</div>
 
